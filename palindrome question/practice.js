5ece814db88afca07c91c49e3fbf3b4f28df0b15
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2023-12-04 23:41:17 +0500

    Add files via upload

---
+++ palindrome question/practice.js
@@ -0,0 +1,16 @@
+// Here I have to check that either my code is palindrome or not , So for this purpose I have to check it in such a way that
+
+let enter = prompt("Enter a number = ");
+
+let find_palindrome_function = (enter) => {
+  let reversed_string = enter.toString().split("").reverse().join("");
+  console.log(reversed_string);
+
+  //   if (enter == reversed_string) {
+  //     alert("It is a palindrome");
+  //   } else {
+  //     alert("It is not a palindrome");
+  //   }
+};
+
+find_palindrome_function(enter);
