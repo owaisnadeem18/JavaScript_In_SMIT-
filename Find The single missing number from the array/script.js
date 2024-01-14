c62bb5944dccd04c06df57f0f7ae051f864b36d7
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-01-14 22:34:26 +0500

    Add files via upload

---
+++ Find The single missing number from the array/script.js
@@ -0,0 +1,48 @@
+// Practical Project Question
+
+// // Find the missing number in an array
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 9, 14, 18, 19, 23];
+
+// for (let a = 0; a < arr.length; a++) {
+//   // for(let i = 1 ; i<=)
+//   console.log(arr[a]);
+// }
+
+// let arr = [1 , 2, 3, 4, 6 , 7 ,9 , 13]
+// let arr = [1, 3, 4, 6, 7, 9, 13];
+
+// for (let a = 0; a <= arr.length - 1; a++) {
+//   if (arr[a] + 1 !== arr[a + 1]) {
+//     let missing_number = arr[a] + 1;
+//     console.log("Missing Number = ", missing_number);
+//     console.log(`I ran ${a} time `);
+//   } else {
+//     console.log("---------------");
+//     console.log("No missing number was present");
+//     console.log(`I ran ${a} time `);
+//   }
+// }
+
+// let arr = [1, 3, 4, 5, 7, 9, 11, 13, 14];
+// console.log(arr);
+// for (i = 0; i < arr.length - 1; i++) {
+//   if (arr[i] + 1 !== arr[i + 1]) {
+//     let missingNum = arr[i] + 1;
+//     console.log("Missing Num: ", missingNum);
+//   }
+// }
+
+// let arr = [1, 4, 8, 9, 25];
+
+// for (let i = 0; i <= arr.length; i++) {
+//   console.log(` I am at first Index =  ${arr[i]} `);
+//   console.log(
+//     ` I am at first Index with addition of 1 in it =  ${arr[i] + 1} `
+//   );
+//   console.log(
+//     ` I am not at first Index , because you have added a number into myself =  ${
+//       arr[i + 1]
+//     } `
+//   );
+// }
