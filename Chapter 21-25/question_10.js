a43662a4beee392786ecde20923555fb86ecab57
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-01-26 02:00:24 +0500

    Add files via upload

---
+++ Chapter 21-25/question_10.js
@@ -0,0 +1,15 @@
+//  Write a program that takes user input. Convert and show the input in capital letters.
+
+// Question # 10
+
+document.write(
+  "------------------------------- Write a program that takes user input. Convert and show the input in capital letters. ------------------------------- <br>"
+);
+document.write(
+  "-------------------------------------- Question # 10 -------------------------------------- <br><br>"
+);
+
+let ask_user = prompt("Enter your input = ");
+
+document.write("The User Input is = ", ask_user, "<br>");
+document.write("The User Input in UpperCase is = ", ask_user.toUpperCase());
