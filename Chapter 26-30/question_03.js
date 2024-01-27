a278eea2816d48eefa33b949315c1a64fefdfcdf
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-01-27 13:33:16 +0500

    Add files via upload

---
+++ Chapter 26-30/question_03.js
@@ -0,0 +1,15 @@
+// Write a program that displays the absolute value of a number.
+// E.g. absolute value of -4 is 4 & absolute value of 5 is 5
+
+document.write(
+  "------------------------------- Write a program that displays the absolute value of a number. E.g. absolute value of -4 is 4 & absolute value of 5 is 5 ------------------------------- <br>"
+);
+document.write(
+  "-------------------------------------- Question # 03 -------------------------------------- <br><br>"
+);
+
+let num = -55;
+
+document.write("The value is: ", num, "<br>");
+
+document.write("The absolute value is: ", Math.abs(num));
