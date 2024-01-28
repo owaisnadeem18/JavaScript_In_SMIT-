4ecb173b3272b4f09251c3cd04d6d3b5d831cf7f
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-01-29 00:31:31 +0500

    Add files via upload

---
+++ Chapter 35-38/question_11.js
@@ -0,0 +1,20 @@
+// Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case. EXAMPLE STRING : 'the quick brown fox' EXPECTED OUTPUT : 'The Quick Brown Fox'
+
+// document.write(
+//   "----------------------------- Question 11 ----------------------------- <br> "
+// );
+
+// document.write(
+//   "----------------------------- Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case. EXAMPLE STRING : 'the quick brown fox' EXPECTED OUTPUT : 'The Quick Brown Fox' ----------------------------- <br><br> "
+// );
+
+let str = "the quick brown fox";
+
+console.log(str.indexOf());
+
+// let func = (a) => {
+//   let output_str = a.indexOf();
+//   console.log(output_str);
+// };
+
+// func(str);
