8dcdd958b83fa2653e717ec592a2fbf912b7dcb5
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-01-28 15:20:37 +0500

    Add files via upload

---
+++ Quiz # 02 Of JavaScript/Chapter (36-40) Day-04/quiz.js
@@ -0,0 +1,4 @@
+let str = "HELLO WORLD";
+let localeLower = str.toLocaleLowerCase();
+
+console.log(localeLower);
---
+++ Quiz # 02 Of JavaScript/Chapter (36-40) Day-04/quiz_02.js
@@ -0,0 +1,40 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// console.log(`The addition of 3 & 5 is = ${add(3, 5)}`);
+// // console.log(`The addition of ${a} & ${b} is = ${add(13, 2)}`);
+// // console.log(`The addition of ${a} & ${b} is = ${add(3, 35)}`);
+// // console.log(`The addition of ${a} & ${b} is = ${add(33, 17)}`);
+
+// The way we can write function in JS using ecma script is:
+
+// let add = (a, b) => {
+//   return a + b;
+// };
+
+// // console.log(add(3, 6));
+// console.log(typeof add(4, 2));
+// console.log(add(4, 2));
+
+// let a = "45";
+// let b = "90";
+
+// console.log(b - a);
+// console.log(b / a);
+// console.log(b * a);
+
+// let a_variable = 78;
+
+// let add = () => {
+//   console.log("Below");
+//   console.log(a_variable);
+//   var a_variable = 89;
+//   console.log(a_variable);
+// };
+
+// console.log(a_variable);
+// add();
+
+let str = "OWAIS";
+console.log(str.toLowerCase());
