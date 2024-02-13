7dc048b56dafd552688833b8f079caf3416c684c
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-02-13 22:01:03 +0500

    Add files via upload

---
+++ JavaScript Competitive Coding Problem Solving/W3 resources JavaScript Problems/question_03.js
@@ -0,0 +1,17 @@
+// getName(fullName) {
+
+// your code here
+
+// output should be
+// first name is : Owais
+// last name is : Nadeem
+// }
+
+let name = "Owais Nadeem";
+
+let Sliced_name = name.split(" ");
+
+console.log(Sliced_name);
+
+console.log("You first name is ", Sliced_name[0]);
+console.log("You second name is ", Sliced_name[1]);
