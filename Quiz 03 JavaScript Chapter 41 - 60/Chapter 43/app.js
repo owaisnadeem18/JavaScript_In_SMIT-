525a67fb079a1edced3e3d8dc2c06f4d6aed9920
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-02-27 23:00:24 +0500

    Add files via upload

---
+++ Quiz 03 JavaScript Chapter 41 - 60/Chapter 43/app.js
@@ -0,0 +1,13 @@
+// Chapter 43 Placing scripts
+
+// it's best to have an external JavaScript file
+
+// All of your JavaScript functions, for example, are loaded into memory when the page loads
+
+// the best place for scripts, though, is at the end of the body section
+
+// main.code.js this is a legal way to create a file of JS
+
+// if the file name contains a space and a question mark, both of which are typically disallowed in file names.
+
+// it's a good idea to put JavaScript code at the end of the body section
