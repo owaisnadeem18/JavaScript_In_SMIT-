ac9f26e214017a729cf54905b57b2a387aa9f866
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-02-27 22:46:41 +0500

    Add files via upload

---
+++ Quiz 03 JavaScript Chapter 41 - 60/Chapter 41/app.js
@@ -0,0 +1,16 @@
+// JavaScript File of Chapter # 41-45
+
+// Chapter # 41 (While Loop)
+
+// for (var i = 1; i <= 3; i++) {
+//   alert(i);
+// }
+
+// But While loop is:
+
+// var i = 1;
+
+// while (i <= 3) {
+//   console.log(i);
+//   i++;
+// }
