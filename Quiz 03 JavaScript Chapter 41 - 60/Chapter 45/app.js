a64b6cf6a5c6c2c0a02f03e1cae31cd5449497e7
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-02-27 23:19:27 +0500

    Add files via upload

---
+++ Quiz 03 JavaScript Chapter 41 - 60/Chapter 45/app.js
@@ -0,0 +1,15 @@
+// Chapter 45 javaScript Alert Function
+
+// you place a # inside the quotation marks. This tells the browser to reload the current page.
+
+// Multi line comment
+
+/*
+    When the button is clicked, execute the following JavaScript." onClick
+isn't case-sensitive. You could write onclick, ONCLICK, or OnClIcK and it would work.
+But the convention is onClick, so we'll stick with that
+*/
+
+function alert_dikhao() {
+  alert("You clicked on the anchor tag");
+}
