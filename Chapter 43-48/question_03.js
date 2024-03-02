c72270bccc6f0d5e1ff496d997148970081a4ab9
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-03 02:04:04 +0500

    Add files via upload

---
+++ Chapter 43-48/question_03.js
@@ -0,0 +1,60 @@
+record_1.addEventListener("click", () => {
+  //   let table_1 = document.getElementById("table_1");
+  //   table_1.style.display = none;
+  let table_1 = document.getElementById("table_1");
+  table_1.style.display = "none";
+});
+record_2.addEventListener("click", () => {
+  //   let table_1 = document.getElementById("table_1");
+  //   table_1.style.display = none;
+  let table_2 = document.getElementById("table_2");
+  table_2.style.display = "none";
+});
+record_3.addEventListener("click", () => {
+  //   let table_1 = document.getElementById("table_1");
+  //   table_1.style.display = none;
+  let table_3 = document.getElementById("table_3");
+  table_3.style.display = "none";
+});
+record_4.addEventListener("click", () => {
+  //   let table_1 = document.getElementById("table_1");
+  //   table_1.style.display = none;
+  let table_4 = document.getElementById("table_4");
+  table_4.style.display = "none";
+});
+record_5.addEventListener("click", () => {
+  //   let table_1 = document.getElementById("table_1");
+  //   table_1.style.display = none;
+  let table_5 = document.getElementById("table_5");
+  table_5.style.display = "none";
+});
+record_6.addEventListener("click", () => {
+  //   let table_1 = document.getElementById("table_1");
+  //   table_1.style.display = none;
+  let table_6 = document.getElementById("table_6");
+  table_6.style.display = "none";
+});
+record_7.addEventListener("click", () => {
+  //   let table_1 = document.getElementById("table_1");
+  //   table_1.style.display = none;
+  let table_7 = document.getElementById("table_7");
+  table_7.style.display = "none";
+});
+record_8.addEventListener("click", () => {
+  //   let table_1 = document.getElementById("table_1");
+  //   table_1.style.display = none;
+  let table_8 = document.getElementById("table_8");
+  table_8.style.display = "none";
+});
+record_9.addEventListener("click", () => {
+  //   let table_1 = document.getElementById("table_1");
+  //   table_1.style.display = none;
+  let table_9 = document.getElementById("table_9");
+  table_9.style.display = "none";
+});
+record_10.addEventListener("click", () => {
+  //   let table_1 = document.getElementById("table_1");
+  //   table_1.style.display = none;
+  let table_10 = document.getElementById("table_10");
+  table_10.style.display = "none";
+});
