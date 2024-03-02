eac60057502db85c06af7ae6ac98080e7c4ba69c
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-03 02:22:08 +0500

    Add files via upload

---
+++ Chapter 43-48/question_04.js
@@ -0,0 +1,13 @@
+// Display an image in browser. Change the picture on mouseover and set the first picture on mouseout.
+
+box = document.getElementById("box");
+
+let changeImg = () => {
+  img.src =
+    "https://png.pngtree.com/thumb_back/fh260/background/20211031/pngtree-abstract-bg-image_914283.png";
+};
+
+let resetImg = () => {
+  img.src =
+    "https://images.pexels.com/photos/255464/pexels-photo-255464.jpeg?cs=srgb&dl=pexels-pixabay-255464.jpg&fm=jpg";
+};
