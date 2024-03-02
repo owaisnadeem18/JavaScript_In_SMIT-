597c497d0c69d6bfa520cb0f5dbefa7af7aab274
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-03 01:07:06 +0500

    Add files via upload

---
+++ Chapter 43-48/question_01.js
@@ -0,0 +1,8 @@
+/* Question 01
+Show an alert box on click on a link */
+
+// I am using an add event listener for showing alert box on clicking at the anchor tag
+
+link.addEventListener("click", () => {
+  alert("hello I have been clicked");
+});
