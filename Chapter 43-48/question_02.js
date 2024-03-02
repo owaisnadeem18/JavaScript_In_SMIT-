0d49085b56fcd6aa6f6822ac8e99e7c7c6831c67
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-03 01:35:13 +0500

    Add files via upload

---
+++ Chapter 43-48/question_02.js
@@ -0,0 +1,12 @@
+img_0.addEventListener("click", () => {
+  alert("Thanks for purchasing our mobile phone");
+});
+img_1.addEventListener("click", () => {
+  alert("Thanks for purchasing our mobile phone");
+});
+img_2.addEventListener("click", () => {
+  alert("Thanks for purchasing our mobile phone");
+});
+img_3.addEventListener("click", () => {
+  alert("Thanks for purchasing our mobile phone");
+});
