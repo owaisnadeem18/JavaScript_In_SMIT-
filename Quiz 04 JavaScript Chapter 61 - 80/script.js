fdf34897e08a1949cc41700f0ca9ca5d667dfd2c
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-10 13:55:37 +0500

    Add files via upload

---
+++ Quiz 04 JavaScript Chapter 61 - 80/script.js
@@ -0,0 +1,53 @@
+// current_date = new Date();
+
+// console.log(current_date); // it will print the local time
+
+// console.log(current_date.getFullYear()); // it will print the current year
+// console.log(current_date.getMonth()); // it will print the current month
+// console.log(current_date.getDay()); // it will print the current month
+
+// let print = (a, b) => {
+//   alert(a);
+// };
+
+// console.log(print(3));
+
+// var x = 0;
+// function foo() {
+//   console.log(x);
+//   x = 10;
+// }
+// foo();
+// // console.log(x);
+
+// Alert ka aik function dekhain
+
+// let a = 56;
+
+// console.log(a != 56);
+
+// var x = 1;
+// function foo() {
+//   //   console.log(x);
+//   return x;
+//   var x = 2;
+
+//   console.log(x);
+// }
+// console.log(foo());
+// console.log(x);   -----------------------> Ask from Sir Sualeh
+
+// var x = 0;
+// function foo() {
+//   return x;
+// }
+// console.log(foo());
+
+// document.write("<h1>Hello World</h1> <b>");
+// document.write("<p>Hello World</p> <b>");
+
+// document.write(4 + 4, "\n");
+// document.write(4 + 4, "\n");
+// document.write(4 + 4, "\n");
+
+document.getElementsByName;
