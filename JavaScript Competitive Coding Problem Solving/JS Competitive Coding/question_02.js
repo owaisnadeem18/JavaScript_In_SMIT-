d89dea25508ce07164943dd3f3c40df2515e4c42
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-13 08:56:54 +0500

    Add files via upload

---
+++ JavaScript Competitive Coding Problem Solving/JS Competitive Coding/question_02.js
@@ -0,0 +1,23 @@
+// <!-- Star pattern program question , we have to solve this question -->
+
+// <!-- Example: -->
+
+/*
+
+*
+**
+***
+****
+*****
+
+*/
+
+// start building the logic for star pattern program
+
+for (let x = 0; x < 5; x++) {
+  let variable = "";
+  for (let y = 0; y <= x; y++) {
+    variable += "*";
+  }
+  console.log(variable);
+}
